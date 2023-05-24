c1332d8f1a697faa76632a5cb2befeac2ae1d7ce
Kevin <kyfeman@gmail.com>
2023-05-24 19:23:50 -0400

    Reconfigured following NixOS Manual

--- flake.nix
+++ flake.nix
@@ -2,48 +2,26 @@
   description = "Halcyon System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   
-  outputs = { nixpkgs, home-manager, ... }: 
-  let 
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-
-    lib = nixpkgs.lib;
-
-  in {
-    homeManagerConfig = {
-      kev = home-manager.lib.homeManagerConfiguration {
-        inherit system pkgs;
-        username = "kev";
-        homeDirectory = "/home/kev";
-        configuration = {
-          imports = [
-            ./home.nix
-          ];
-        };
-      };
-    };
-    
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      halcyon = lib.nixosSystem {
-        inherit system;
-
+      halcyon = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          # Pin nixpkgs so nix commands don't redownload the registry every time
-#          { 
-#            nix.registry.self.flake = nixpkgs;
-#          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kev= import ./home.nix;
+          }
         ];
       };
     };
   };
 }
+ 

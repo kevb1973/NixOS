173f19df857880004def7f018104163b157792ce
Kevin <kyfeman@gmail.com>
2023-05-26 12:24:33 -0400

    Simplify sound setup

--- configuration.nix
+++ configuration.nix
@@ -170,12 +170,13 @@
   services.flatpak.enable = true;
   
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+#  sound.enable = true;
+#  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
+#    alsa.enable = true;
+#    alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;

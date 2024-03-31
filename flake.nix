ac821578c75dcce5f416327982e650e7c58ce9cc
Kevin <kyfeman@gmail.com>
2024-03-31 12:28:17 -0400

    Disable trunk/stable inputs while not needed
    Save downloading multiple versions of nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-trunk.url = "github:nixos/nixpkgs";
+    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    # nixpkgs-trunk.url = "github:nixos/nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
     };
   };
 

20130705a25d1e08408bba7691345c82058bf2c1
Kevin <kyfeman@gmail.com>
2024-08-09 02:43:12 -0400

    Setup gtk in HM

--- modules/environment.nix
+++ modules/environment.nix
@@ -4,9 +4,9 @@
     pathsToLink = [ "/libexec" ]; # enable polkit
     # --- ETC{{{2
     etc = {
-      "xdg/gtk-3.0".source = ./gtk-3.0;
-      "xdg/gtk-4.0".source = ./gtk-4.0;
-      "xdg/wallpaper".source = ./wallpaper;
+      # "xdg/gtk-3.0".source = ./gtk-3.0;
+      # "xdg/gtk-4.0".source = ./gtk-4.0;
+      # "xdg/wallpaper".source = ./wallpaper;
       "nix/inputs/nixpkgs".source =
         "${inputs.nixpkgs}"; # needed to fix <nixpkgs> on flake. See also nix.nixPath
     };
--- modules/home.nix
+++ modules/home.nix
@@ -31,4 +31,23 @@
     };
     mpdris2.enable = true;
   };
+  dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome-themes-extra;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
+  };
 } #end home.nix

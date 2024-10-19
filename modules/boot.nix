af0a987ffc4df805eb38b010dd87d4e5e53a7230
Kevin <kyfeman@gmail.com>
2024-10-19 01:06:40 -0400

    Enable Plymouth for boot

--- modules/boot.nix
+++ modules/boot.nix
@@ -10,10 +10,15 @@
     swraid.enable = false; # Setting needed as system state ver < 23.11
     initrd = {
       kernelModules = [ "amdgpu" ];
-      systemd.network.wait-online.enable = false;
+      systemd = {
+        enable = true;
+        network.wait-online.enable = false;
+      };
+      verbose = false; #for silent boot (plymouth)
     };
     loader = {
       systemd-boot.enable = true;
+      timeout = 0; #for silent boot (plymouth)
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot";
@@ -31,11 +36,22 @@
       #"amd_pstate=active"
       # "nowatchdog"
       # "nmi_watchdog=0"
-      "quiet"
+      "quiet" #for silent boot (plymouth)
+      "splash" #for silent boot (plymouth)
+      "boot.shell_on_fail" #for silent boot (plymouth)
+      "loglevel=3" #for silent boot (plymouth)
+      "rd.systemd.show_status=false" #for silent boot (plymouth)
+      "rd.udev.log_level=3" #for silent boot (plymouth)
+      "udev.log_priority=3" #for silent boot (plymouth)
       "amd_iommu=on"
       "iommu=pt"
       "systemd.mask=systemd-vconsole-setup.service" # Added due to VConsole error on boot. See https://github.com/NixOS/nixpkgs/issues/312452#issuecomment-2320993345
       "systemd.mask=dev-tpmrm0.device"
     ];
+    plymouth = {
+      enable = true;
+    };
+    consoleLogLevel = 0; #for silent boot (plymouth)
+    
   };
 }

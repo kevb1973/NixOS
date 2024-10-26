9c09e7ffa110c12819889dcb6a1b3df4578cb48b
Kevin <kyfeman@gmail.com>
2024-10-26 00:56:40 -0400

    May remove this code.. testing config without it.
    I believe this (or similar) behaviour has been made automatic. See
    https://github.com/NixOS/nixpkgs/pull/254405/files#diff-d8c48bdf5db8c63b4221f4f9664402a20b5abf38a0ff57b10faea3a7a80ef1ff
    
    Commenting out for now.. will remove later if no issues.

--- flake.nix
+++ flake.nix
@@ -4,13 +4,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixos-cli.url = "github:water-sucks/nixos";
-    nixos-cli.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-cli.url = "github:water-sucks/nixos";
+    nixos-cli.inputs.nixpkgs.follows = "nixpkgs";
     # helix.url = "github:helix-editor/helix";
     # niri.url = "github:sodiboo/niri-flake";
-    # rust-overlay.follows = ""; #disable unneeded dev stuff foro niri flake
+    # rust-overlay.follows = ""; #disable unneeded dev stuff for niri flake
     # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
     # nixpkgs-trunk.url = "github:nixos/nixpkgs";
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
@@ -18,7 +18,6 @@
     # hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
     # hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
     # hyprland-plugins.inputs.hyprland.follows = "hyprland";
-    # waybar.url = "github:Alexays/Waybar";
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, nixos-cli, ... }: {
@@ -31,15 +30,15 @@
           nixos-cli.nixosModules.nixos-cli
           # niri.nixosModules.niri
           # nixos-cosmic.nixosModules.default
-          ({ pkgs, ... }: {
-            nix.registry.sys = {
-              from = {
-                type = "indirect";
-                id = "sys";
-              };
-              flake = nixpkgs;
-            };
-          })
+          # ({ pkgs, ... }: { # I believe this function is no longer needed.. remove if no issues.
+          #   nix.registry.sys = {
+          #     from = {
+          #       type = "indirect";
+          #       id = "sys";
+          #     };
+          #     flake = nixpkgs;
+          #   };
+          # })
           home-manager.nixosModules.home-manager
           {
             home-manager = {

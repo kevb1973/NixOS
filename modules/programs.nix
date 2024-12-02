1b5a5480c4c6a1dfb13d6040e38c45fc42c064b8
Kevin <kyfeman@gmail.com>
2024-12-02 14:11:02 -0500

    Change lp alias

--- modules/programs.nix
+++ modules/programs.nix
@@ -47,7 +47,7 @@
         gcroots = "sudo nix-store --gc --print-roots | grep -Ev '^(/proc|/nix|/run)'";
         lg = "lazygit";
         logout = "sudo systemctl restart display-manager.service";
-        lp = "nix profile list | grep -E 'Name|Store'";
+        lp = "nix profile list";
         lu = ''echo -e "\n\e[1mLast Flake Update:\e[0m $(ls -l ~/NixOS/flake.lock | awk '{print $6, $7, $8}')\n"'';
         no = "nixos option -i";
         np = "nh search"; # search nix packages

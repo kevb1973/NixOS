9341ad7634f792e70f3e365c7a42b1a0f71e85fa
Kevin <kyfeman@gmail.com>
2024-12-21 16:30:42 -0500

    Add myself to trusted users for for helix cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -27,6 +27,7 @@
         # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" 
         # "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
+      trusted-users = [ "kev" ];
     };
   };
 }

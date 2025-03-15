c1a8372747ad34deaa0fc307d6aae2785e7f3073
Kevin <kyfeman@gmail.com>
2025-03-15 19:48:41 -0400

    Disable niri and nixos-cli for now

--- flake.nix
+++ flake.nix
@@ -3,16 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-cli.url = "github:water-sucks/nixos";
-    nixos-cli.inputs.nixpkgs.follows = "nixpkgs";
-    # helix.url = "github:helix-editor/helix";
-    niri.url = "github:sodiboo/niri-flake";
-    rust-overlay.follows = ""; #disable unneeded dev stuff for niri flake
+    # nixos-cli.url = "github:water-sucks/nixos";
+    # nixos-cli.inputs.nixpkgs.follows = "nixpkgs";
+    # niri.url = "github:sodiboo/niri-flake";
+    # rust-overlay.follows = ""; #disable unneeded dev stuff for niri flake
+    # nixpkgs.follows = "nixos-cosmic/nixpkgs";
     # nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
-    # nixpkgs-trunk.url = "github:nixos/nixpkgs";
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     # hyprland-contrib.url = "github:hyprwm/contrib";
     # hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
@@ -20,15 +18,15 @@
     # hyprland-plugins.inputs.hyprland.follows = "hyprland";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-cli, niri, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       halcyon = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          nixos-cli.nixosModules.nixos-cli
-          niri.nixosModules.niri
+          # nixos-cli.nixosModules.nixos-cli
+          # niri.nixosModules.niri
           # nixos-cosmic.nixosModules.default
           home-manager.nixosModules.home-manager
           {

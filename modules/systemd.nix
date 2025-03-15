ded74bc1f49afe1540a9573ce79d24fd62391819
Kevin <kyfeman@gmail.com>
2025-03-15 19:47:30 -0400

    Disable.. doesn't work

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -7,16 +7,16 @@
     network.wait-online.enable = false; # Disable systemd "wait online" as it gets stuck waiting for connection on 2nd NIC
     services = {
       NetworkManager-wait-online.enable = false;
-      numLockOnTty = {
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
-            for tty in /dev/tty{1..6}; do
-                ${pkgs.kbd}/bin/setleds -D +num < "$tty";
-            done
-          '');
-        };
-      };
+      # numLockOnTty = {
+      #   wantedBy = [ "multi-user.target" ];
+      #   serviceConfig = {
+      #     ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+      #       for tty in /dev/tty{1..6}; do
+      #           ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+      #       done
+      #     '');
+      #   };
+      # };
     };
     tpm2.enable = false; #see https://www.reddit.com/r/NixOS/comments/1hazcra/nixos_takes_forever_to_boot_suddenly/
     # tmpfiles.rules = [

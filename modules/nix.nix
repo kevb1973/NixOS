745017e4c5c4a0c4bbf04511a51c94a4c28b0241
Kevin <kyfeman@gmail.com>
2025-05-06 23:32:15 -0400

    Enable niri cachix

--- modules/nix.nix
+++ modules/nix.nix
@@ -24,10 +24,12 @@
       max-jobs = 2;
       substituters = [
         "https://hyprland.cachix.org"
+        "https://niri.cachix.org"
         # "https://cosmic.cachix.org/"
       ];
       trusted-public-keys = [ 
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" 
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         # "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
       trusted-users = [ "kev" ]; # Needed to use substituters with nix profile install/upgrade

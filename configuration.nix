b4088a96198263750b1b2d29a2cf454220a448a1
Kevin <kyfeman@gmail.com>
2025-05-10 00:49:08 -0400

    Try mandoc instead of man-db

--- configuration.nix
+++ configuration.nix
@@ -22,4 +22,9 @@
   time.timeZone = "America/Toronto";
   i18n.defaultLocale = "en_CA.UTF-8";
   system.stateVersion = "22.11"; # Don't change unless fresh install from new ISO
+
+  documentation.man = {
+    man-db.enable = false;
+    mandoc.enable = true;
+  };
 }

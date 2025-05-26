96c3a1fbaf9b37760945786c3f4ddb3e4f502cc1
Kevin <kyfeman@gmail.com>
2025-05-25 23:25:11 -0400

    Remove deprecated option

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -3,7 +3,6 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
-      rocmSupport = false;
     # permittedInsecurePackages = [ "electron-25.9.0" ];
     };
     overlays = [ inputs.niri.overlays.niri ];

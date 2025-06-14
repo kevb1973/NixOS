c11595f98c942b96e63c751e68f88c70f559b396
Kevin <kyfeman@gmail.com>
2025-06-14 01:41:18 -0400

    Didn't know about corefonts..

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -4,11 +4,15 @@
     enableDefaultPackages = true;
     fontDir.enable = true;
     packages = with pkgs; [
+      corefonts
       noto-fonts-lgc-plus
       nerd-fonts.victor-mono
       maple-mono.NF
       font-awesome
       noto-fonts-color-emoji
     ];
+    fontconfig.defaultFonts = {
+      monospace = [ "VictorMono Nerd Font" ];
+    };
   };
 }

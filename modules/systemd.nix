329398c02640f6425d64de3df7db73a83f7597eb
Kevin <kyfeman@gmail.com>
2025-08-03 14:14:36 -0400

    systemd extraConfig -> systemd.settings.Manager

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -1,12 +1,10 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   systemd = {
     coredump.enable = false;
-    extraConfig = ''
-      DefaultTimeoutStopSec=10s
-    '';
     network.wait-online.enable = false; # Disable systemd "wait online" as it gets stuck waiting for connection on 2nd NIC
     services = {
       NetworkManager-wait-online.enable = false;
@@ -20,13 +18,16 @@
         TTYVTDisallocate = true;
       };
     };
+    settings.Manager = {
+      DefaultTimeoutStopSec = "10s";
+    };
 
     # timers.podman-auto-update = {
     #   enable = true;
     #   timerConfig = { OnCalendar = "03:00"; };
     # };
-    
-    tpm2.enable = false; #see https://www.reddit.com/r/NixOS/comments/1hazcra/nixos_takes_forever_to_boot_suddenly/
+
+    tpm2.enable = false; # see https://www.reddit.com/r/NixOS/comments/1hazcra/nixos_takes_forever_to_boot_suddenly/
 
     tmpfiles.rules = [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"

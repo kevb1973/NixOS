6bed2cc18f3f0238a3661b68c253d7c4c721e7f5
Kevin <kyfeman@gmail.com>
2025-08-08 21:02:15 -0400

    Update virtualization settings

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,4 +1,4 @@
-_:
+{ pkgs, ... }:
 {
   virtualisation = {
     containers.enable = true;
@@ -8,13 +8,24 @@ _:
       dockerSocket.enable = true;
     };
     # oci-containers.backend = "podman";
-    
+
     libvirtd = {
       enable = true;
       onBoot = "ignore";
       onShutdown = "shutdown";
       qemu = {
+        package = pkgs.qemu_kvm;
         runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
       };
     };
   };

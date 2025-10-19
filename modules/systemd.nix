9003a0fe9aebed21b5dde5230283eb6463119c28
Kev <kyfeman@gmail.com>
2025-10-19 02:43:57 -0400

    Cleanup + Fix for vconsole-setup

--- modules/systemd.nix
+++ modules/systemd.nix
@@ -1,5 +1,5 @@
 {
-  pkgs,
+  # pkgs,
   ...
 }:
 {
@@ -8,24 +8,11 @@
     network.wait-online.enable = false; # Disable systemd "wait online" as it gets stuck waiting for connection on 2nd NIC
     services = {
       NetworkManager-wait-online.enable = false;
-      # greetd.serviceConfig = {
-      #   Type = "idle";
-      #   StandardInput = "tty";
-      #   StandardOutput = "tty";
-      #   StandardError = "journal";
-      #   TTYReset = true;
-      #   TTYVHangup = true;
-      #   TTYVTDisallocate = true;
-      # };
+      systemd-vconsole-setup.after = [ "local-fs.target" ]; # fix slow boot caused by vconsole-setup
     };
     settings.Manager = {
       DefaultTimeoutStopSec = "10s";
     };
-
-    # timers.podman-auto-update = {
-    #   enable = true;
-    #   timerConfig = { OnCalendar = "03:00"; };
-    # };
 
     tpm2.enable = false; # see https://www.reddit.com/r/NixOS/comments/1hazcra/nixos_takes_forever_to_boot_suddenly/
 

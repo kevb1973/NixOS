d3cadc4539f6d0f10aecc1a10ccdc39456c50fb8
Kev <kyfeman@gmail.com>
2025-11-07 00:10:40 -0500

    Created a script to toggle system theme Separate scripts no longer needed.

--- home/dots/bin/darkmode.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-dms ipc theme dark
--- home/dots/bin/lightmode.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
-dms ipc theme light

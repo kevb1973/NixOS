6d8b95a7e2678fdefd15a6eaa66d21164e649fe3
Kev <kyfeman@gmail.com>
2025-11-08 08:37:33 -0500

    Check jj st for changes before splitting

--- home/dots/fish/functions/js.fish
+++ home/dots/fish/functions/js.fish
@@ -1,10 +1,10 @@
 function js -d "Run jj split until ctrl-c"
     while true
-        jj split
-        if test $status
+        if test (jj st | head -1) = "The working copy has no changes."
             echo "Nothing to split!"
             break
         end
+        jj split
         snore 2
     end
 end

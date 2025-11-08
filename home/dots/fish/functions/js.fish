374c12f48dc6667d7768312d62b0e9c8825fa624
Kev <kyfeman@gmail.com>
2025-11-08 08:28:37 -0500

    Check if nothing to split, exit

--- home/dots/fish/functions/js.fish
+++ home/dots/fish/functions/js.fish
@@ -1,7 +1,10 @@
 function js -d "Run jj split until ctrl-c"
     while true
-
         jj split
+        if test $status
+            echo "Nothing to split!"
+            break
+        end
         snore 2
     end
 end

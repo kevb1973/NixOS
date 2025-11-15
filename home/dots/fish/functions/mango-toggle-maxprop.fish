537599044fc380b7172a829ff6f932d6c3ae5aef
Kev <kyfeman@gmail.com>
2025-11-14 23:47:21 -0500

    Toggle fullscreen for mangowc There is a built in function, but when you use it, it covers other windows.

---
+++ home/dots/fish/functions/mango-toggle-maxprop.fish
@@ -0,0 +1,8 @@
+function mango-toggle-maxprop -d "Toggle between full and half-screen in MangoWC"
+    set current_width (mmsg -g | grep width | awk '{print $3}')
+    if test "$current_width" -gt 1270
+        mmsg -d set_proportion,.5
+    else
+        mmsg -d set_proportion,1
+    end
+end

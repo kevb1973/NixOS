6932f48484941b93dba1b33b8a3101d4bcbfee2e
Kev <kyfeman@gmail.com>
2025-12-01 06:33:35 -0500

    Add polkit rule for udiskie running under systemd

--- home/home.nix
+++ home/home.nix
@@ -106,7 +106,7 @@
     mpdris2.enable = true;
     udiskie = {
       enable = true;
-      tray = "never";
+      tray = "auto";
     };
   };
 
--- modules/security.nix
+++ modules/security.nix
@@ -1,7 +1,31 @@
 _:
 {
   security = {
-    polkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          var YES = polkit.Result.YES;
+          var permission = {
+            // required for udisks2:
+            "org.freedesktop.udisks2.filesystem-mount": YES,
+            "org.freedesktop.udisks2.encrypted-unlock": YES,
+            "org.freedesktop.udisks2.eject-media": YES,
+            "org.freedesktop.udisks2.power-off-drive": YES,
+            // required for udisks2 if using udiskie from another seat (e.g. systemd):
+            "org.freedesktop.udisks2.filesystem-mount-other-seat": YES,
+            "org.freedesktop.udisks2.filesystem-unmount-others": YES,
+            "org.freedesktop.udisks2.encrypted-unlock-other-seat": YES,
+            "org.freedesktop.udisks2.encrypted-unlock-system": YES,
+            "org.freedesktop.udisks2.eject-media-other-seat": YES,
+            "org.freedesktop.udisks2.power-off-drive-other-seat": YES
+          };
+          if (subject.isInGroup("storage")) {
+            return permission[action.id];
+          }
+        });
+      '';
+    };
     rtkit.enable = false;
     sudo.extraRules = [
       {

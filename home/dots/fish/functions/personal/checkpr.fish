632a8f8d6969b33ae0d67d6a767af54cb263d61f
Kev <kyfeman@gmail.com>
2026-01-02 22:30:11 -0500

    Re-write checkpr function + add to `launch` - can be called with or without a PR# - if called without, use gum to request it - if called with, just output the results - replace `nu` with `jq`.. more ubiquitous - improve output so it's pretty :-)

--- home/dots/fish/functions/personal/checkpr.fish
+++ home/dots/fish/functions/personal/checkpr.fish
@@ -1,8 +1,23 @@
 function checkpr
-    if test -n "$argv"
-        # collect results for specified PR and format with jq
-        nu -c "curl -s -L https://nixpkgs.molybdenum.software/api/v2/landings/$argv | from json"
+    clear
+    # If no param provided, use gum to get PR#
+    if test -z $argv
+        set -f pr_input (gum input --header="Nixpkgs PR Merge Checker" --placeholder="#123456")
     else
-        echo "Please enter a PR number without '#'"
+        set -f pr_input $argv
+    end
+
+    # Remove hash if it's there
+    set pr_number (string replace -r '^#' '' -- $pr_input[1])
+
+    # Pull json data and format/output with jq
+    echo -e "\nPull Request #$pr_number - Merged Branches"
+    echo "----------------------------------------"
+    curl -s -L "https://nixpkgs.molybdenum.software/api/v2/landings/$pr_number" |
+        jq -r '.branches[] | "Branch: \(.)"'
+
+    # If being called from script (popup terminal), pause or it will disappear.
+    if test -z $argv
+        echo; gum choose --header="" exit
     end
 end

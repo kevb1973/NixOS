92c758337e74a986337bb7df2a7e33b96274f1d9
Kev <kyfeman@gmail.com>
2026-01-09 03:10:21 -0500

    Setup helix config for steel plugins

---
+++ home/dot-config/helix/init.scm
@@ -0,0 +1,10 @@
+(require "smooth-scroll/smooth-scroll.scm")
+(require "scooter/scooter.scm")
+(require "mattwparas-helix-package/cogs/recentf.scm")
+(require "steel-pty/term.scm")
+
+;; Start the snapshotting in the background.
+(recentf-snapshot)
+
+;;(set-default-shell! "/run/current-system/sw/bin/fish")
+

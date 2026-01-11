6e387ef6570b1c3b995738384ccc93f2832b2bd4
Kev <kyfeman@gmail.com>
2026-01-10 20:49:26 -0500

    Add new fish script to mass untrack/ignore Uses `tv` to allow selection

--- home/dot-config/fish/functions/personal/ignore-untrack.fish
+++ home/dot-config/fish/functions/personal/ignore-untrack.fish
@@ -1,15 +1,21 @@
 function ignore-untrack --description "Ignore/untrack new files in jj repo"
     cd ~/NixOS
     # check if there are untracked files
-    if test (jj status | grep '^A ')
-        # use gum to select files to ignore/untrack
-        set -f selected_files (jj status | grep '^A.*' | gum choose --header "Add to .gitignore" --no-limit | sed 's/^..//')
-        if test $selected_files
+    set -f untracked_files (jj status | grep '^A ')
+    if test -n "$untracked_files"
+        # use tv to select files to ignore/untrack
+        set -f selected_files "$(tv jj-new)"
+        if test -n "$selected_files"
             # add selected files to .gitignore and then untrack
+            echo -------------------------------------
+            echo "Ignore/Untrack the following files?"
+            echo -------------------------------------
             echo "$selected_files"
-            # tee -a .gitignore | xargs jj file untrack
+            gum confirm && echo "$selected_files" | tee -a .gitignore | xargs jj file untrack
         end
     else
-        echo "\nNo untracked files found"
+        echo ------------------------
+        echo "No untracked files found"
+        echo ------------------------
     end
 end

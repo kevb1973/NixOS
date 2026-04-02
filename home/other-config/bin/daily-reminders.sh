6d4496539b796769e21e7169600fb37b01056bdc
Kev <kyfeman@gmail.com>
2026-04-01 23:46:32 -0400

    Update flake

--- home/other-config/bin/daily-reminders.sh
+++ home/other-config/bin/daily-reminders.sh
@@ -2,7 +2,7 @@
 # atrm $(atq | cut -f1)
 # Setup reminder notifications
 
-echo "notify-snooze --urgency=critical 'Take Iodine & Allergy Pill?'" | at 4:00pm
+echo "notify-snooze --urgency=critical 'Take Iodine & Allergy Pill?'" | at 10:00am
 echo "notify-snooze --urgency=critical 'Time for Melatonin/Snack'" | at 11:30pm
 echo "notify-snooze --urgency=critical 'Time for Bed!'" | at 12:00am
 echo "daily-reminders.sh" | at 12:01am

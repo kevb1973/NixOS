14a26018ae5a25396ab718ab392aea2feb3eaf28
Kev <kyfeman@gmail.com>
2026-05-13 05:19:00 -0400

    Format flake.nix (probably zed..)

--- flake.nix
+++ flake.nix
@@ -7,35 +7,36 @@
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-      };
-
+    };
 
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    # determinate,
-    ...
-  }@inputs: {
-    nixosConfigurations = {
-      halcyon = nixpkgs.lib.nixosSystem rec {
-        specialArgs = {inherit system inputs;};
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          # determinate.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.kev = ./home/home.nix;
-              extraSpecialArgs = { inherit inputs; };
-            };
-          }
-        ];
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      # determinate,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        halcyon = nixpkgs.lib.nixosSystem rec {
+          specialArgs = { inherit system inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            # determinate.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.kev = ./home/home.nix;
+                extraSpecialArgs = { inherit inputs; };
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }

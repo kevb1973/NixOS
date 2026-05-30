120b029f314ac8ced2cc5fba0704dd91cb23c13c
Kev <kyfeman@gmail.com>
2026-05-30 16:03:36 -0400

    Edit daily-reminders

--- home/other-config/bin/daily-reminders.sh
+++ home/other-config/bin/daily-reminders.sh
@@ -2,9 +2,10 @@
 # atrm $(atq | cut -f1)
 # Setup reminder notifications
 
-echo "notify-snooze --urgency=critical 'Take Iodine & Allergy Pill?'" | at 1:00pm
-echo "notify-snooze --urgency=critical 'Take 3tbsp Olive Oil'" | at 10:00pm
+echo "notify-snooze --urgency=critical 'Take Iodine & Allergy Pill'" | at 1:00pm
+echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 1:00pm
+echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 7:00pm
 echo "notify-snooze --urgency=critical 'Time for Melatonin/Snack'" | at 11:30pm
 echo "notify-snooze --urgency=critical 'Time for Bed!'" | at 12:00am
-echo "daily-reminders.sh" | at 12:01am
-# notify-send "Reminders have been set."
+echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 1:00am
+echo "daily-reminders.sh" | at 1:01am # reset for next day..

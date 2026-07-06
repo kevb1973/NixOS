db69ff8664386dbc06aecc42823462155089ef63
Kev <kyfeman@gmail.com>
2026-07-06 19:37:40 -0400

    Edit some scripts - remove expire time from notify-snooze - remove unneeded reminders

--- home/other-config/bin/daily-reminders.sh
+++ home/other-config/bin/daily-reminders.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
-# atrm $(atq | cut -f1)
 # Setup reminder notifications
 
-echo "notify-snooze --urgency=critical 'Take Iodine & Allergy Pill'" | at 1:00pm
-echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 1:00pm
-echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 7:00pm
-echo "notify-snooze --urgency=critical 'Time for Melatonin/Snack'" | at 11:30pm
-echo "notify-snooze --urgency=critical 'Time for Bed!'" | at 12:00am
-echo "notify-snooze --urgency=critical 'Take 1tbsp Olive Oil'" | at 1:00am
-echo "daily-reminders.sh" | at 1:01am # reset for next day..
+echo "notify-snooze 'Take Iodine & Allergy Pill'" | at 1:00pm
+echo "notify-snooze 'Time for Melatonin/Snack'" | at 11:30pm
+echo "notify-snooze 'Time for Bed!'" | at 12:00am
+echo "daily-reminders.sh" | at 12:01am # reset for next day..

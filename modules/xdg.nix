aced2ede641ce0bf8d7b1ce920de828eeb68e20f
Kev <kyfeman@gmail.com>
2026-08-11 23:35:22 -0400

    Remove unneeded portals

--- modules/xdg.nix
+++ modules/xdg.nix
@@ -4,13 +4,11 @@
     portal = {
       config.common.default = "*"; # Needed for xdg-desktop-portal
       enable = true;
+      xdgOpenUsePortal = true;
       extraPortals = with pkgs; [
         xdg-desktop-portal-wlr
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-gnome
-        # kdePackages.xdg-desktop-portal-kde
       ];
-      wlr.enable = true;
     };
     mime = {
       enable = true;

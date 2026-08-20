623e8eeba9fd927c435d62f2f5a47e65001d59a2
Kev <kyfeman@gmail.com>
2026-08-19 20:41:30 -0400

    Remove reminder - getting off the allergy pills..

--- home/other-config/bin/daily-reminders.sh
+++ home/other-config/bin/daily-reminders.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Setup reminder notifications
 
-echo "notify-snooze 'Take Iodine & Allergy Pill'" | at 1:00pm
+echo "notify-snooze 'Take Iodine'" | at 1:00pm
 echo "notify-snooze 'Time for Melatonin/Snack'" | at 11:30pm
 echo "notify-snooze 'Time for Bed!'" | at 12:00am
 echo "daily-reminders.sh" | at 12:01am # reset for next day..

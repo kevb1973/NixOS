10fb361ea6a3e1d22ae7904a83c14a1627c6133a
Kev <kyfeman@gmail.com>
2026-09-07 18:58:44 -0400

    Change `js` to `jsplit` - Avoid conflicting with `js` - Use ~/NixOS repo by default

--- home/dot-config/fish/functions/personal/jsplit.fish
+++ home/dot-config/fish/functions/personal/jsplit.fish
@@ -1,11 +1,14 @@
-function js -d "Run jj split until ctrl-c"
+function jsplit -d "Run jj split in NixOS config until ctrl-c"
     while true
+        # Check if there are changes remaining
         if test (jj st | head -1) = "The working copy has no changes."
             echo "All Done!"
             break
         end
-        jj split 2>/dev/null
+        jj split -R ~/NixOS/ 2>/dev/null
+        # handle 'quit'
         if test $status -ne 0
+            echo "Split Cancelled"
             break
         end
     end

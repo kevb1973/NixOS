173215145912b78d9b7eab5d8a1b025e8dd3d181
Kev <kyfeman@gmail.com>
2026-09-09 16:28:59 -0400

    Fix `jsplit` function - Original testing method was error prone   due to color formatting in `jj st` output - Checking jj diff will return nothing if there are no changes

--- home/dot-config/fish/functions/personal/jsplit.fish
+++ home/dot-config/fish/functions/personal/jsplit.fish
@@ -1,12 +1,12 @@
 function jsplit -d "Run jj split in NixOS config until ctrl-c"
     while true
-        # Check if there are changes remaining
-        if test (jj st | head -1) = "The working copy has no changes."
+        # With fish, if there is nothing returned, test will fail
+        # string collect makes sure there is a string to test, even if empty
+        if test -z (jj diff -R ~/NixOS/ --summary | string collect)
             echo "All Done!"
             break
         end
         jj split -R ~/NixOS/ 2>/dev/null
-        # handle 'quit'
         if test $status -ne 0
             echo "Split Cancelled"
             break
